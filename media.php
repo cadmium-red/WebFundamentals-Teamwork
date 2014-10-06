8e07a92d46e3bf30bc5e667c097984ead83c4c12
Lyubomir Markov <liubomir.markov@gmail.com>
2014-10-06 13:33:23 +0300

    Revert "Revert "Add gallery to media.php and fix text in footer.""
    This reverts commit ba5a420c1a586ab0c40e95401bf741d5396df7b8.

--- media.php
+++ media.php
@@ -23,17 +23,27 @@
 			
 			<div id="content"> <!-- Start CONTENT (Article) -->
 			
-			<h2>Media</h2>
-			
-			<p>
-				Image
-			</p>
-			
-			<p>
-			
-			Text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text.
-			</p>
-			
+				<!-- Gallery -->
+				<div class="gallery">
+					<a href="http://placehold.it/600x450/4D99E0/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/4D99E0/ffffff.png&text=200x200" width="200" height="200" /></a>
+					<a href="http://placehold.it/600x450/75CC00/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/75CC00/ffffff.png&text=200x200" width="200" height="200" /></a>
+					<a href="http://placehold.it/600x450/E49800/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/E49800/ffffff.png&text=200x200" width="200" height="200" /></a>
+					<a href="http://placehold.it/600x450/E4247E/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/E4247E/ffffff.png&text=200x200" width="200" height="200" /></a>
+					<a href="http://placehold.it/600x450/00C6E4/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/00C6E4/ffffff.png&text=200x200" width="200" height="200" /></a>
+					<a href="http://placehold.it/600x450/E4DB0F/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/E4DB0F/ffffff.png&text=200x200" width="200" height="200" /></a>
+					<a href="http://placehold.it/600x450/7400E4/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/7400E4/ffffff.png&text=200x200" width="200" height="200" /></a>
+					<a href="http://placehold.it/600x450/C50000/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/C50000/ffffff.png&text=200x200" width="200" height="200" /></a>
+					<a href="http://placehold.it/600x450/C50000/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/C50000/ffffff.png&text=200x200" width="200" height="200" /></a>
+					<a href="http://placehold.it/600x450/C50000/ffffff.png&text=600x450"><img src="http://placehold.it/100x100/C50000/ffffff.png&text=200x200" width="200" height="200" /></a>
+				</div>
+				<!-- Align Left -->
+				<img class="align-left" src="http://placehold.it/600x450/7400E4/ffffff.png&text=600x450" width="600" height="450" />
+				<h2>Title ot image</h2>
+				<p>Text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, 
+				text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, 
+				text, text, text, text, text, text, text, text, text, text, text, text, text, 
+				text, text, text, text, text, text, text, text, text, text, text, text, text, text, 
+				text, text, text, text, text, text, text, text, text, text, text, text, text, text, text!</p>
 			</div> <!-- end #content -->
 			
 			<!-- START NAVIGATION -->
