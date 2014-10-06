a3e68b7599f46e2ad9f057132ae07deed80ad9d8
krastoss <servicemail945@gmail.com>
2014-10-06 23:05:30 +0300

    updated phps

--- about.php
+++ about.php
@@ -37,13 +37,10 @@
 			</p>
 			
 			</div> <!-- end #content -->
-			
-			<!-- START NAVIGATION -->
+		</div> <!-- End #wrapper -->			
+			<!-- START FOOTER -->
 			<?php include('includes/footer.php'); ?>
-			<!-- END NAVIGATION -->
-			
-		</div> <!-- End #wrapper -->
-
+			<!-- END FOOTER -->
 	</body>
-
 </html>
+
