3db74c5a3e1afac4434fdd769c753dcd3127d003
krastoss <servicemail945@gmail.com>
2014-10-06 14:05:00 +0300

    added navbar

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,24 @@
 <div id="nav">
-	<a href="index.php">Introduction</a>
-	<a href="about.php">About</a>
-	<a href="media.php">Media</a>
-	<a href="facilities.php">Facilities</a>
+<!-- Menu Horizontal -->
+<ul class="menu">
+<li class="current"><a href="index.php">Introduction</a></li>
+<li><a href="about.php">About</a></li>
+<li><a href="media.php">Media</a></li>
+<li><a href="facilities.php">Facilities</a></li>
+<li><a href=""><i class="icon-inbox"></i>About</a>
+	<ul>
+	<li><a href="media.php"><i class="icon-cog"></i>Media</a></li>
+	<li><a href=""><i class="icon-envelope-alt"></i> Sub Item</a>
+		<ul>
+		<li><a href=""><i class="icon-wrench"></i> Sub Item</a></li>
+		<li><a href=""><i class="icon-camera-retro"></i> Sub Item</a></li>
+		<li><a href=""><i class="icon-coffee"></i> Sub Item</a></li>
+		<li><a href=""><i class="icon-twitter"></i> Sub Item</a></li>
+		</ul>
+	</li>
+	<li class="divider"><a href=""><i class="icon-trash"></i> li.divider</a></li>
+	</ul>
+</li>
+<li><a href="">Item 4</a></li>
+</ul>
 </div>
