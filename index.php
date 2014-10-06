deb16b85a619e4cc8e983e860efb50bc4f7b1844
krastoss <servicemail945@gmail.com>
2014-10-06 09:45:36 +0300

    edited php files to mark HTML as HTML5

--- index.php
+++ index.php
@@ -1,45 +1,34 @@
 <!DOCTYPE html>
 
 <html>
-
 <head>
-
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-
 <meta name="description" content="" />
-
 <meta name="keywords" content="" />
-
 <meta name="author" content="" />
-
 <link rel="stylesheet" type="text/css" href="css\style.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="css\kickstart-slideshow.css" media="screen" />
 <link rel="stylesheet" type="text/css" href="css\kickstart.css" media="screen" />
-
 <title>Idea - Complete Idiot's guide to SoftUni</title>
-
 </head>
-
 	<body>
 		<!-- NAVIGATION -->
 		<?php include('includes/nav.php'); ?>
 		<!-- NAVIGATION -->
-		<div id="wrapper">
-			
-			<div id="header">
-			
-				<h2>Slider - featured Test blabla</h2>
-			
+		<div id="wrapper">			
+			<div id="header">			
+				<!-- Slideshow -->
+				<div>
+					<ul class="slideshow">
+					<li><img src="http://placehold.it/550x350/4D99E0/ffffff.png&text=550x350" width="550" height="350" /></li>
+					<li><img src="http://placehold.it/550x350/75CC00/ffffff.png&text=550x350" width="550" height="350" /></li>
+					<li><img src="http://placehold.it/550x350/E49800/ffffff.png&text=550x350" width="550" height="350" /></li>
+					<li><h3>Slide Anything</h3><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
+					sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p></li>
+					</ul>
+				</div>
 			</div> <!-- end #header -->
-			<div>						<!-- Slideshow -->
-				<ul class="slideshow">
-				<li><img src="http://placehold.it/550x350/4D99E0/ffffff.png&text=550x350" width="550" height="350" /></li>
-				<li><img src="http://placehold.it/550x350/75CC00/ffffff.png&text=550x350" width="550" height="350" /></li>
-				<li><img src="http://placehold.it/550x350/E49800/ffffff.png&text=550x350" width="550" height="350" /></li>
-				<li><h3>Slide Anything</h3><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-				sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p></li>
-				</ul>
-			</div>
+
 			<div id="content"> <!-- Start CONTENT (Article) -->
 			
 			<h2>About article - Introduction to SoftUni Organization</h2>
