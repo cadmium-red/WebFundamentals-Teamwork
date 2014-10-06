14a767a3f9e3541d999212611f8dd1b108919dca
Lyubomir Markov <liubomir.markov@gmail.com>
2014-10-06 13:04:51 +0300

    Revert "fix style.css"
    This reverts commit 22243daf96b0ba78deb72e7f25463629638cc09b.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <meta name="author" content="" />	
 <!-- CSS -->
 <link rel="stylesheet" type="text/css" href="css/kickstart.css" media="all" />
-<link rel="stylesheet" type="text/css" href="css/style.css" media="all" /> 	
+<link rel="stylesheet" type="text/css" href="style.css" media="all" /> 	
 	<!-- Javascript -->
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript" src="js/kickstart.js"></script>
