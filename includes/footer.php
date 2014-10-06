d5f32c9f6e053caf92426ddbbc9518a496237689
Lyubomir Markov <liubomir.markov@gmail.com>
2014-10-06 13:07:45 +0300

    Add 2 columns in footer for text and facebook box

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,16 @@
 <div id="footer">
-	<p>Footer (box for facebook and etc. Test)</p>
+	<div class="col_6 visible column">
+		<h3>ЗА УНИВЕРСИТЕТА</h3>
+		<ul>
+			<li>За нас</li>
+			<li>Професии</li>
+			<li>Планове</li>
+			<li>Университети-партньори</li>
+			<li>Преподаватели</li>
+			<li>Общежития</li>
+			<li>Кандидатстване</li>
+		<ul>
+	</div>
+	<div class="col_6 visible column">Footer (box for facebook and etc. Test)</div>
+	<p></p>
 </div> 
