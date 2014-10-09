f802257853b7e93fda2d0767e957cf7ba0ebaa77
krastoss <servicemail945@gmail.com>
2014-10-09 15:03:30 +0300

    resolved footer conflict

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 ﻿<div id="footer" class="col_12">
 	<div class="col_6 visible column">
-=======
-﻿<div class="clear"></div>
-<div id="footer">
-	<div>
->>>>>>> origin/master
 		<h3>ЗА УНИВЕРСИТЕТА</h3>
 		<ul>
 			<li>За нас</li>
