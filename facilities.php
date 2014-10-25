7b6ce86c395cca62604825276499e75b8845d906
krastoss <servicemail945@gmail.com>
2014-10-25 14:47:39 +0300

    logo

--- facilities.php
+++ facilities.php
@@ -1,43 +1,109 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<!-- META -->
-<title>Facilites</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<meta name="description" content="" />
-<meta name="keywords" content="" />
-<meta name="author" content="" />	
-<!-- CSS -->
-<link rel="stylesheet" type="text/css" href="css/kickstart.css" media="all" />
-<link rel="stylesheet" type="text/css" href="css/style.css" media="all" /> 	
-	<!-- Javascript -->
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/kickstart.js"></script>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Facilities</title>
+
+    <!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" /> 	
+
 </head>
-	<body>
-		<!-- NAVIGATION -->
-		<?php include('includes/nav.php'); ?>
-		<!-- NAVIGATION -->
-		<div class="grid">			
-			<div id="content"> <!-- Start CONTENT (Article) -->
-			
-			<h2>Facilites</h2>
-			
-			<p>
-				Image
-			</p>
-			
-			<p>
-			
-			Text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text, text.
-			</p>
-			
-			</div> <!-- end #content -->
-		</div> <!-- End #wrapper -->			
+
+<body>
+
+	<!-- NAVIGATION -->
+	<?php include('includes/nav.php'); ?>
+	<!-- NAVIGATION -->
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Portfolio Item Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Portfolio Item
+                    <small>Item Subheading</small>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Item Row -->
+        <div class="row">
+
+            <div class="col-md-8">
+                <img class="img-responsive" src="http://placehold.it/750x500" alt="">
+            </div>
+
+            <div class="col-md-4">
+                <h3>Project Description</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+                <h3>Project Details</h3>
+                <ul>
+                    <li>Lorem Ipsum</li>
+                    <li>Dolor Sit Amet</li>
+                    <li>Consectetur</li>
+                    <li>Adipiscing Elit</li>
+                </ul>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <!-- Related Projects Row -->
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h3 class="page-header">Related Projects</h3>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
 			<!-- START FOOTER -->
 			<?php include('includes/footer.php'); ?>
 			<!-- END FOOTER -->
-	</body>
-</html>
 
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.0 -->
+    <script src="js/jquery-1.11.0.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
--- includes/nav.php
+++ includes/nav.php
@@ -8,12 +8,14 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
+            <a class="navbar-brand" href="#">
+                    <img src="img/logo.png" alt="">
+                </a>
+		</div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="facilities.php">Facilities</a></li>
             <li><a href="media.php">Media</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
