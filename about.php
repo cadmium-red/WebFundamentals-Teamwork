9b8a6bece959c3eeabdd3cee38043f9b7d7b654b
krastoss <servicemail945@gmail.com>
2014-10-26 09:46:18 +0200

    added modal to about.php

--- about.php
+++ about.php
@@ -41,19 +41,37 @@
                 <h2 class="page-header">Our Team</h2>
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="img/gizmo-thumb.png" alt="">
+                <img class="img-circle img-responsive img-center" src="img/gizmo-thumb.png" alt="gizmoImg" data-toggle="modal" data-target="#gizmoModal">
                 <h3>Гизмо Наков
                     <small>Master Могуей</small>
                 </h3>
                 <p>Наков не се страхува от нищо! Но всичко се страхува от Наков!</p>
             </div>
+			<div id="gizmoModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="aboutModal" aria-hidden="true">
+			  <div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-body">
+						<img src="img/gizmo.png" class="img-responsive">
+					</div>
+				</div>
+			  </div>
+			</div>			
             <div class="col-lg-4 col-sm-6 text-center">
-                <img class="img-circle img-responsive img-center" src="img/achebg-thumb.png" alt="">
+                <img class="img-circle img-responsive img-center" src="img/achebg-thumb.png" alt="achebgImg" data-toggle="modal" data-target="#acheModal">
                 <h3>Аче Смит
                     <small>Java Могуей</small>
                 </h3>
                 <p>Джаварите не включват светлината...Те гасят тъмнината</p>
             </div>
+			<div id="acheModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="aboutModal" aria-hidden="true">
+			  <div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-body">
+						<img src="img/achebg.png" class="img-responsive">
+					</div>
+				</div>
+			  </div>
+			</div>
             <div class="col-lg-4 col-sm-6 text-center">
                 <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
                 <h3>John Smith
