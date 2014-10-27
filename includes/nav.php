e3c2115774b17db873e245ace4d79c73e745f8ac
krastoss <servicemail945@gmail.com>
2014-10-27 20:46:43 +0200

    edited nav

--- includes/nav.php
+++ includes/nav.php
@@ -15,22 +15,10 @@
         <div class="collapse navbar-collapse ">
           <ul class="nav navbar-nav navbar-right">
             <li class="active"><a href="index.php">Начало</a></li>
-            <li><a href="facilities.php">Помещения</a></li>
+			<li><a href="about.php">Екип</a></li>
+            <li><a href="facilities.php">За Нас</a></li>
             <li><a href="media.php">Галерия</a></li>
-            <li><a href="about.php">За ХардУни</a></li>
             <li><a href="contacts.php">Контакти</a></li>
-            <!--<li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>-->
           </ul>
         </div><!--/.nav-collapse -->
       </div>
