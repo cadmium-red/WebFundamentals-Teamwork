75bdd2fb514159deed6e5e588d82471e0fac39cb
Lyubomir Markov <liubomir.markov@gmail.com>
2014-10-27 18:26:25 +0200

    Add images to gallery

--- media.php
+++ media.php
@@ -39,8 +39,8 @@
     <div class="row">
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Изморени и мислещи за биричка по-време на 12та-та лекция за деня." href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6332.JPG"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Който не слуша на четири крака ще си ходи!" href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6359.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Който не слуша на четири крака ще си ходи! От Боровец до вкъщи." href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6360.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Здрав дух здраво тяло" href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6363.JPG"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="От Боровец до вкъщи." href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6360.JPG"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Един софтуерен инженер трябва да поддържа и физическата си форма!" href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6363.JPG"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 5" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/2255EE"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 6" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/449955/FFF"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 8" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/777"></a></div>
@@ -48,7 +48,7 @@
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 10" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/EEE"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 11" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/449955/FFF"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 12" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/DDD"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 13" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/992233"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Тълпа ентусиазирани студенти на HardUni се тълпят пред най-новите хардуерни технологии (OLinuXino)" href="#"><img class="thumbnail img-responsive" src="gallery/DSC08690.JPG"></a></div>
     </div>
     <hr>
   </div>
