5db625bb9957605853039337d80489c225659001
Lyubomir Markov <liubomir.markov@gmail.com>
2014-10-27 19:08:52 +0200

    Add gallery to img! :)

--- media.php
+++ media.php
@@ -37,18 +37,18 @@
   <div class="row">
     <h1>Гледки от живота в ХардУни</h1>
     <div class="row">
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Изморени и мислещи за биричка по-време на 12та-та лекция за деня." href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6332.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Който не слуша на четири крака ще си ходи!" href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6359.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="От Боровец до вкъщи." href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6360.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Един софтуерен инженер трябва да поддържа и физическата си форма!" href="#"><img class="thumbnail img-responsive" src="gallery/IMG_6363.JPG"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Изморени и мислещи за биричка по-време на 12та-та лекция за деня." href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6332.JPG"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Който не слуша на четири крака ще си ходи!" href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6359.JPG"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="От Боровец до вкъщи." href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6360.JPG"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Един софтуерен инженер трябва да поддържа и физическата си форма!" href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6363.JPG"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 5" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/2255EE"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 6" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/449955/FFF"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 8" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/777"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 9" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/992233"></a></div>
       <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 10" href="#"><img class="thumbnail img-responsive" src="//placehold.it/600x350/EEE"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="На колко изпита те скъсаха? Ако броим и утрешния, пет." href="#"><img class="thumbnail img-responsive" src="gallery/fb_img_01.jpg"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="В HardUni лекциите продължават и в малките часове на нощта!" href="#"><img class="thumbnail img-responsive" src="gallery/DSC08875.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Тълпа ентусиазирани студенти на HardUni се тълпят пред най-новите хардуерни технологии (OLinuXino)" href="#"><img class="thumbnail img-responsive" src="gallery/DSC08690.JPG"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="На колко изпита те скъсаха? Ако броим и утрешния, пет." href="#"><img class="thumbnail img-responsive" src="img/gallery/fb_img_01.jpg"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="В HardUni лекциите продължават и в малките часове на нощта!" href="#"><img class="thumbnail img-responsive" src="img/gallery/DSC08875.JPG"></a></div>
+      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Тълпа ентусиазирани студенти на HardUni се тълпят пред най-новите хардуерни технологии (OLinuXino)" href="#"><img class="thumbnail img-responsive" src="img/gallery/DSC08690.JPG"></a></div>
     </div>
     <hr>
   </div>
