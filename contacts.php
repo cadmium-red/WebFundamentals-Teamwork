0348ba512c37e75b3ead5eb4ec8b740ee1a9a508
Lyubomir Markov <liubomir.markov@gmail.com>
2014-10-27 19:43:57 +0200

    Add contacts.php and remove scripts

---
+++ contacts.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- META -->
+<title>Media</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="author" content="" />	
+<!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" /> 	
+</head>
+	<body>
+		    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/scripts.js"></script>	
+
+		<!-- NAVIGATION -->
+		<?php include('includes/nav.php'); ?>
+		<!-- NAVIGATION -->
+		<div class="grid">
+			<div id="header">			
+    <!-- Page Content -->
+<div class="container">
+    
+	<div class="col-md-8">
+		<h1>Свържете се с нас</h1>
+		pesho ti siii :D
+	</div>
+	<div class="col-md-4">
+	<div class="fb-like-box" data-href="https://www.facebook.com/pages/HardUni/814806185224174" data-colorscheme="dark" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
+	</div>
+</div>
+    <!-- /.container -->
+			<!-- START FOOTER -->
+			<?php include('includes/footer.php'); ?>
+			<!-- END FOOTER -->
+	</body>
+</html>
