b82185bd5d1082e1879604014cd18d89b2f41a51
krastoss <servicemail945@gmail.com>
2014-10-27 18:46:57 +0200

    fix

--- includes/footer.php
+++ includes/footer.php
@@ -2,34 +2,7 @@
       <div class="container">
 		<div class="row show-grid">
 			<div class="col-md-4">
-				<div class="col span_2_of_3">
-                <div class="contact-form">
-                    <h3>Contact us:</h3>
-                    <form method="post" action="phoContact/contact.php" onsubmit="MM_validateForm(&#39;name&#39;,&#39;&#39;,&#39;R&#39;,&#39;email&#39;,&#39;&#39;,&#39;R&#39;
-,&#39;phone&#39;,&#39;&#39;,&#39;R&#39;,&#39;msg&#39;,&#39;&#39;,&#39;R&#39;)return document.MM_returnValue" encytype="text/plain, charset=UTF-8">
-                        <div class="input-group">
-                            <span class="input-group-addon" ><span class="glyphicon glyphicon-user"></span></span></span>
-                            <span><input name="name" type="text" class="form-control" placeholder="Type your name here" required="required"></span>
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-                            <span><input name="email" type="text" class="form-control" placeholder="Type your e-mail here" required="required"></span>
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon"><span class="glyphicon glyphicon-earphone"></span></span>
-                            <span><input name="phone" type="text" class="form-control" placeholder="Type your phone here" required="required"></span>
-                        </div>
-                        <div class="input-group">
-                            <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                            <span><textarea class="form-control" placeholder="Type your message here.." name="msg" required="required"></textarea></span>
-                        </div>
-                        <div>
-                            <span><input class="btn btn-primary btn-lg" type="submit" value="Send"></span>
-                        </div>
-                    </form>
-
-                </div>
-            </div>
+				<p class="text-muted">2014 Копирайт ХардУни. Олл райдс ризърветед!</p>
 			</div>
 			<div class="col-md-4">
 				<h2>ХардУни</h2>
@@ -43,9 +16,7 @@
 			<div class="col-md-4">
 				<div class="fb-like-box" data-href="https://www.facebook.com/pages/HardUni/814806185224174" data-colorscheme="dark" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
 			</div>
-			
 		</div>
         
       </div>
-	  <p class="text-muted">2014 Копирайт ХардУни. Олл райдс ризърветед!</p>
     </div>
