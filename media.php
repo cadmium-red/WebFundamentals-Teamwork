e2b0c939ddab3f733a07660b8e76667bfb1b7197
krastoss <servicemail945@gmail.com>
2014-10-27 23:13:25 +0200

    gallery validation fix

--- media.php
+++ media.php
@@ -18,58 +18,57 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
 	<script src="js/scripts.js"></script>	
-
+		<div id="header">
 		<!-- NAVIGATION -->
 		<?php include('includes/nav.php'); ?>
+		</div>
 		<!-- NAVIGATION -->
-		<div class="grid">
-			<div id="header">			
     <!-- Page Content -->
-<div class="container">
-  <div class="row">
-    <h1>Гледки от живота в ХардУни</h1>
-    <div class="row">
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Изморени и мислещи за биричка по-време на 12та-та лекция за деня." href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6332.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Който не слуша на четири крака ще си ходи!" href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6359.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="От Боровец до вкъщи." href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6360.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Един софтуерен инженер трябва да поддържа и физическата си форма!" href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6363.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 5" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life1.jpg"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 6" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life2.jpg"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="В HardUni разбира се се намира и по някоя нормална душа. :)" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life3.jpg"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 9" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life4.jpg"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 10" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life5.jpg"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="На колко изпита те скъсаха? Ако броим и утрешния, пет." href="#"><img class="thumbnail img-responsive" src="img/gallery/fb_img_01.jpg"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="В HardUni лекциите продължават и в малките часове на нощта!" href="#"><img class="thumbnail img-responsive" src="img/gallery/DSC08875.JPG"></a></div>
-      <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Тълпа ентусиазирани студенти на HardUni се тълпят пред най-новите хардуерни технологии (OLinuXino)" href="#"><img class="thumbnail img-responsive" src="img/gallery/DSC08690.JPG"></a></div>
-    </div>
-    <hr>
-  </div>
-</div>
-<div class="modal" id="myModal" role="dialog">
-  <div class="modal-dialog">
-  <div class="modal-content">
-	<div class="modal-header">
-		<button class="close" type="button" data-dismiss="modal">×</button>
-		<h3 class="modal-title"></h3>
-	</div>
-	<div class="modal-body">
-		<div id="modalCarousel" class="carousel">
- 
-          <div class="carousel-inner">
-           
-          </div>
-          
-          <a class="carousel-control left" href="#modaCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
-          <a class="carousel-control right" href="#modalCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
-          
-        </div>
-	</div>
-	<div class="modal-footer">
-		<button class="btn btn-default" data-dismiss="modal">Close</button>
-	</div>
-   </div>
-  </div>
-</div>
+			<div class="container">
+			  <div class="row">
+				<h1>Гледки от живота в ХардУни</h1>
+				<div class="row">
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Изморени и мислещи за биричка по-време на 12та-та лекция за деня." href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6332.JPG" alt="tired"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Който не слуша на четири крака ще си ходи!" href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6359.JPG" alt="four legs"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="От Боровец до вкъщи." href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6360.JPG"  alt="crawling"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Един софтуерен инженер трябва да поддържа и физическата си форма!" href="#"><img class="thumbnail img-responsive" src="img/gallery/IMG_6363.JPG" alt="physical form"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 5" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life1.jpg" alt="HardUni Img1"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 6" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life2.jpg"  alt="HardUni Img2"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="В HardUni разбира се се намира и по някоя нормална душа. :)" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life3.jpg" alt="HardUni Img3"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 9" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life4.jpg" alt="HardUni Img4"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Image 10" href="#"><img class="thumbnail img-responsive" src="img/gallery/harduni_life5.jpg" alt="HardUni Img5"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="На колко изпита те скъсаха? Ако броим и утрешния, пет." href="#"><img class="thumbnail img-responsive" src="img/gallery/fb_img_01.jpg" alt="HardUni Img6"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="В HardUni лекциите продължават и в малките часове на нощта!" href="#"><img class="thumbnail img-responsive" src="img/gallery/DSC08875.JPG" alt="HardUni Img7"></a></div>
+				  <div class="col-lg-3 col-sm-4 col-xs-6"><a title="Тълпа ентусиазирани студенти на HardUni се тълпят пред най-новите хардуерни технологии (OLinuXino)" href="#"><img class="thumbnail img-responsive" src="img/gallery/DSC08690.JPG" alt="HardUni Img8"></a></div>
+				</div>
+				<hr>
+			  </div>
+			</div>
+		<div class="modal" id="myModal" role="dialog">
+		  <div class="modal-dialog">
+		  <div class="modal-content">
+			<div class="modal-header">
+				<button class="close" type="button" data-dismiss="modal">×</button>
+				<h3 class="modal-title">Галерия</h3>
+			</div>
+			<div class="modal-body">
+				<div id="modalCarousel" class="carousel">
+		 
+				  <div class="carousel-inner">
+				   
+				  </div>
+				  
+				  <a class="carousel-control left" href="#modaCarousel" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+				  <a class="carousel-control right" href="#modalCarousel" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+				  
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		   </div>
+		  </div>
+		</div>
     <!-- /.container -->
 			<!-- START FOOTER -->
 			<?php include('includes/footer.php'); ?>
