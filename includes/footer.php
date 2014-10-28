1058201f022b28099ce2ec690bcc07ad1bde985e
krastoss <servicemail945@gmail.com>
2014-10-28 20:17:15 +0200

    footer and css - for validation

--- includes/footer.php
+++ includes/footer.php
@@ -5,8 +5,8 @@
 				<p class="text-muted">2014 Копирайт ХардУни. Олл райдс ризърветед!</p>
 			</div>
 			<div class="col-md-6 text-right">
-				<a class="text-danger" href="https://www.facebook.com/pages/HardUni/814806185224174?ref=bookmarks"><i id="social" class="fa fa-facebook-square fa-3x social-fb"></i></a>
-				<a class="text-danger" href="https://softuni.bg/"><i id="social" class="fa fa-laptop fa-3x social-fb"></i></a>
+				<a class="text-danger" href="https://www.facebook.com/pages/HardUni/814806185224174?ref=bookmarks"><i id="social-fb-icon" class="fa fa-facebook-square fa-3x social-fb"></i></a>
+				<a class="text-danger" href="https://softuni.bg/"><i id="social-site-icon" class="fa fa-laptop fa-3x social-fb"></i></a>
 			</div>
 		</div>        
       </div>
